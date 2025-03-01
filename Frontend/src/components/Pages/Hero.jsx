7d86b90b480266d0d9e9fbf5dab1fbdf811beb34
SURESH-S-U <suresh.elite07@gmail.com>
2025-03-01 10:22:18 +0530

    Hero page  completed

--- Frontend/src/components/Pages/Hero.jsx
+++ Frontend/src/components/Pages/Hero.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import "../Styles/Hero.css";
+import { TiPlusOutline } from "react-icons/ti";
+
 
 const Hero = () => {
   return (
-    <div className="hero-wrapper"> {/* Parent Wrapper */}
-      <div className="hero-container">
-        <h4 className="hero-title">SURESH S U</h4>
-        <h1 className="hero-subtitle">
-          Web Developer
-          <div className="loop">
-            <Typewriter
-              words={["AI Engineer", "UI&UX Designer", "DATA Engineer"]}
-              loop={Infinity}
-              cursor
-              typeSpeed={100}
-              deleteSpeed={50}
-              delaySpeed={1000}
-              className="slide-in-text"
-            />
-          </div>
-        </h1>
-      </div>
+    <div className="hero-container">
+      <h4 className="hero-title">SURESH S U</h4>
+      <h1 className="hero-subtitle">
+        Web Developer   <span className="plus"><TiPlusOutline size={50} color="gold"/></span> &nbsp; &nbsp; &nbsp;
+        <div className="loop">
+          &nbsp; &nbsp;&nbsp;
+          <Typewriter
+            words={["AI Engineer", "UI&UX Designer", "DATA Engineer"]}
+            loop={Infinity}
+            cursor
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            className="slide-in-text"
+          />
+        </div>
+      </h1>
     </div>
   );
 };
