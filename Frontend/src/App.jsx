aab1eaab9f0ba36334c95e3488a3ad41fc649159
SURESH-S-U <suresh.elite07@gmail.com>
2025-03-01 10:28:40 +0530

    Scroll bar removed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Pages/Hero';
 import Resume from './components/Pages/Resume';
 import Projects from './components/Pages/Projects';
 import Contact from './components/Pages/Contact';
+import "./App.css";
 
 const App = () => {
   return (
